12950b28d7db4c5112013d29a657e87fb7422562
asnganje <abd.nganje@gmail.com>
2024-09-03 18:36:14 +0300

    Create social items list

---
+++ src/utils/socialIcons.ts
@@ -0,0 +1,35 @@
+import { sIcons } from "./types";
+import { FaFacebook, FaXTwitter, FaSquareInstagram, FaYoutube, FaLinkedin } from "react-icons/fa6";
+
+export const socialIcons: sIcons[] = [
+  {
+    id: 1,
+    href: "#",
+    icon: FaFacebook,
+    color: "text-blue-500"
+  },
+  {
+    id: 2,
+    href: "#",
+    icon: FaXTwitter,
+    color: "text-black dark:text-white"
+  },
+  {
+    id: 3,
+    href: "#",
+    icon: FaSquareInstagram,
+    color: "text-pink-700"
+  },
+  {
+    id: 4,
+    href: "#",
+    icon: FaYoutube,
+    color: "text-red-500"
+  },
+  {
+    id: 5,
+    href: "#",
+    icon: FaLinkedin,
+    color: "text-blue-700"
+  },
+]
