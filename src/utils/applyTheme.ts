40eb51e5585023443410bc63d0fda03527e94b80
asnganje <abd.nganje@gmail.com>
2024-09-03 08:38:15 +0300

    Add function for applying theme

---
+++ src/utils/applyTheme.ts
@@ -0,0 +1,13 @@
+import { Theme } from "@/features/theme/themeSlice";
+
+export function applyTheme(theme:Theme) {
+  const root = window.document.documentElement;
+  root.classList.remove('light', 'dark')
+
+  if (theme === 'system') {
+    const systemTheme = window.matchMedia('(prefers-color-scheme:dark)').matches?'dark':'light';
+    root.classList.add(systemTheme)
+    return
+  }
+  root.classList.add(theme)
+}
