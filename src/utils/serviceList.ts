cabdb2fe1aa8e155884d3a0889c326a764d2813d
asnganje <abd.nganje@gmail.com>
2024-09-03 18:36:29 +0300

    Add services list

---
+++ src/utils/serviceList.ts
@@ -0,0 +1,24 @@
+import { Service } from "./types";
+
+export const service: Service[] = [
+  {
+    id:1,
+    description:"Dolphin Tour"
+  },
+  {
+    id:2,
+    description:"Prison Island Tour"
+  },
+  {
+    id:3,
+    description:"Jozani Forest Tour"
+  },
+  {
+    id:4,
+    description:"Nungwi Village Tour"
+  },
+  {
+    id:8,
+    description:"Sunset Cruising"
+  },
+]
