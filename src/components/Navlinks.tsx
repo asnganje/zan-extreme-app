72909cbbc5d966c5a94fe6dc7e9ac0db97072755
asnganje <abd.nganje@gmail.com>
2024-09-03 08:39:41 +0300

    Create navlinks component

--- src/components/Navlinks.tsx
+++ src/components/Navlinks.tsx
@@ -1,6 +1,23 @@
+import { links } from "@/utils"
+import { NavLink } from "react-router-dom"
+
 function Navlinks() {
   return (
-    <div>Navlinks</div>
+    <div className="hidden lg:flex justify-center items-center gap-x-8">
+      {
+        links.map((link)=> {
+          return <NavLink 
+                  key={link.label}
+                  to={link.href}
+                  className={({isActive})=> {
+                    return `capitalize font-light tracking-wide hover:font-semibold ${isActive? 'text-primary' : ''}`
+                  }}
+                >
+                  {link.label}
+            </NavLink>
+        })
+      }
+    </div>
   )
 }
 export default Navlinks
