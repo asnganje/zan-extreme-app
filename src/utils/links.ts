024dec23f86b836964fd32ae6d53c7baf6d1ba6a
asnganje <abd.nganje@gmail.com>
2024-09-03 07:31:34 +0300

    Add list of links with href and label properties

---
+++ src/utils/links.ts
@@ -0,0 +1,12 @@
+type Link = {
+  href: string;
+  label: string;
+};
+
+export const links: Link[] = [
+  { href: '/', label: 'home' },
+  { href: 'about', label: 'about' },
+  { href: 'products', label: 'products' },
+  { href: 'services', label: 'Our services' },
+  { href: 'contact', label: 'Contact Us' },
+];
