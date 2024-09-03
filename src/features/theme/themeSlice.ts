7683f3173a7829808e13fea7b6b389c3072b6f8e
asnganje <abd.nganje@gmail.com>
2024-09-03 08:38:34 +0300

    Create theme state using redux

---
+++ src/features/theme/themeSlice.ts
@@ -0,0 +1,34 @@
+import { applyTheme } from "@/utils";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export type Theme = 'dark' | 'light' | 'system' 
+
+type ThemeState = {
+  theme: Theme;
+}
+
+const initializeTheme = ():Theme => {
+  const theme = localStorage.getItem('theme') as Theme || 'system'
+  applyTheme(theme)
+  return theme;
+
+}
+
+const initialState: ThemeState = {
+  theme: initializeTheme()
+}
+
+const themeSlice = createSlice({
+  name:'theme',
+  initialState,
+  reducers:{
+    setTheme:(state, action:PayloadAction<Theme>)=> {
+      state.theme = action.payload;
+      applyTheme(action.payload);
+      localStorage.setItem('theme', action.payload)
+    }
+  }
+})
+
+export const themeReducer = themeSlice.reducer;
+export const {setTheme} = themeSlice.actions;
