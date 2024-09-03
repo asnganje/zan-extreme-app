89f68295bb722bba802ec1ac8addc79af9932be3
asnganje <abd.nganje@gmail.com>
2024-09-03 18:35:54 +0300

    Add contact type and create contact list

---
+++ src/utils/contacts.ts
@@ -0,0 +1,20 @@
+import { MapPin, MailOpen, PhoneCall } from "lucide-react";
+import { Contact } from "./types";
+
+export const contact: Contact[] = [
+  {
+    id:1,
+    icon: MapPin,
+    info: "Magomeni Wandaras, Zanzibar"
+  },
+  {
+    id:2,
+    icon: MailOpen,
+    info: "zanextremeholydays@gmail.com"
+  },
+  {
+    id:3,
+    icon: PhoneCall,
+    info: "+255 653 118 899"
+  },
+]
---
+++ src/utils/types.ts
@@ -0,0 +1,19 @@
+import { LucideIcon } from "lucide-react";
+
+export type Contact = {
+  id:number;
+  icon: LucideIcon
+  info:string;
+}
+
+export type sIcons = {
+  id:number;
+  href:string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  color:string;
+} 
+
+export type Service = {
+  id: number;
+  description: string;
+}
