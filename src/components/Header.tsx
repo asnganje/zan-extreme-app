cb9eb6b9730e4520c342fda956c5ba13a5ce5aa7
asnganje <abd.nganje@gmail.com>
2024-09-03 09:02:08 +0300

    Added framer motion logic to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,29 @@
+import { motion } from 'framer-motion';
+
 function Header() {
   return (
-    <h3>Welcome to Zan Extreme! | +254723663237 | twahangenya@gmail.com</h3>
-  )
+    <motion.div
+      initial={{ x: '100vw', opacity: 0 }}
+      animate={{
+        x: '0vw',
+        opacity: 1,
+      }}
+      transition={{
+        x: {
+          duration: 4,
+          ease: 'linear',
+        },
+        opacity: {
+          duration: 1,
+          ease: 'easeOut',
+        },
+      }}
+    >
+      <h3 className="flex items-center justify-end text-blue-700">
+        +254723663237 | zanextremeholydays@gmail.com
+      </h3>
+    </motion.div>
+  );
 }
-export default Header
+
+export default Header;
