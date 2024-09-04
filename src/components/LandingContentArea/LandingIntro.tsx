16d1d70956a8dadd331559024c1a405ca43424b6
asnganje <abd.nganje@gmail.com>
2024-09-04 10:44:38 +0300

    Modify logo styling

--- src/components/LandingContentArea/LandingIntro.tsx
+++ src/components/LandingContentArea/LandingIntro.tsx
@@ -7,11 +7,11 @@ function LandingIntro() {
         <p className="font-bodoni text-4xl text-blue-500 font-extrabold text-center">Zan Extreme Holidays and Wildlife Safaris!</p>
       </div>
       <div className="flex flex-col items-center md:grid md:grid-cols-[30%,1fr]">
-        <div className="w-[90%] h-full md:w-[20vw] md:h-[30vh] lg:h-[35vh] md:mb-[5vh]">
+        <div className="w-[90%] h-full xs:w-[80%] xs:h-[90%] md:w-[20vw] md:h-[30vh] lg:h-[35vh] md:py-[5vh]">
           <Logo />
         </div>
         <div className="my-4">
-            <p className="px-8 text-center md:py-8 shadow font-montserrat text-gray-500 tracking-wide">
+            <p className="px-8 text-center md:text-start md:py-8 shadow font-montserrat text-gray-500 dark:text-white tracking-wide">
               Welcome to the Enchanting Spice Island of Zanzibar and the Untamed Wilderness of Tanzania! 
               Immerse yourself in the exotic allure of Zanzibar, the fabled Spice Islands, and the breathtaking landscapes of Tanzania's wildlife havens. With over 15 years of expertise in Tourism and Hospitality, we are thrilled to introduce ZAN EXTREME HOLIDAYS AND WILDLIFE SAFARIS, your gateway to unparalleled adventures.
             </p>
--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "/zan1.png"
+import logo from "/logo.png"
 
 function Logo() {
   return (
