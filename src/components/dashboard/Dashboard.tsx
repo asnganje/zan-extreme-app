6384d3e1897d12414040d648d5754df2e58ac2be
asnganje <abd.nganje@gmail.com>
2024-09-04 12:57:57 +0300

    modify cursor to pointer

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -9,12 +9,12 @@ function Dashboard() {
           return (
             <div key={item.id} className={`flex flex-col md:grid md:grid-cols-2 md:gap-[5vw] mb-[15vh]`}>
               <div className={`flex flex-col ${i % 2 === 0 ? 'md:order-2' : 'md:order-1'}`}>
-                <p className="text-blue-300 text-2xl">{item.heading}</p>
+                <p className="text-blue-300 text-2xl cursor-pointer">{item.heading}</p>
                 <Separator className="text-orange-400"/>
                 <p className="px-8 text-center md:text-start md:py-8 shadow font-montserrat text-gray-500 dark:text-white tracking-wide">{item.text}</p>
             </div>
             <div className={`flex justify-center items-center ${i % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
-              <img src={item.image} alt="" />
+              <img src={item.image} alt="" className="cursor-pointer"/>
             </div>
             </div>
           )
