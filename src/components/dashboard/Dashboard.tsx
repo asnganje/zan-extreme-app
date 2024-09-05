77131abed3a1d4c74da0855bae9a96dedf512d5c
asnganje <abd.nganje@gmail.com>
2024-09-05 10:37:33 +0300

    Add logic for toggling text visibility

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,29 +1,44 @@
 import { dashItems } from "@/utils/dashItems"
 import { Separator } from "../ui/separator"
-// import { useState } from "react"
+import { useState } from "react"
 
 function Dashboard() {
-  // const [isVisible, setIsVisible] = useState<number[]>([])
+  const [isVisible, setIsVisible] = useState<number[]>([])
 
-  // const toggleVisibility = (id: number) => {
-  //   setIsVisible((prevIds)=> prevIds.includes(id)?
-  //                 prevIds.filter((prevId)=>prevId !== id):
-  //                 [...prevIds, id]
-  // )
+  const toggleVisibility = (id: number) => {
+    setIsVisible((prevIds)=> prevIds.includes(id)?
+                  prevIds.filter((prevId)=>prevId !== id):
+                  [...prevIds, id]
+    )
+  }
 
-  // }
+  const truncateText = (text:string, limit: number) => {
+    if (text.length > 0) {
+      return text.substring(0, limit)+'...'
+    }
+    return text
+  }
+
   return (
     <div className="shadow-2xl my-[12vh] p-8">
       <p className="text-center uppercase text-xl xs:text-3xl text-blue-400 px-1 py-6 shadow-2xl rounded mt-2 mb-10"><span className="hidden md:inline">"Discover Zanzibar: </span><span className="inline md:hidden">"THE </span>Wonders Tour Package<span className="inline md:hidden">"</span><span className="hidden md:inline">"</span></p>
       {
         dashItems.map((item, i)=> {
+          const isFullTextVisible = isVisible.includes(item.id)
+          const textLimit = 300;
           return (
             <div key={item.id} className={`flex flex-col md:grid md:grid-cols-2 md:gap-[5vw] mb-[15vh]`}>
               <div className={`flex flex-col ${i % 2 === 0 ? 'md:order-2' : 'md:order-1'}`}>
                 <p className="text-blue-300 text-center text-2xl cursor-pointer">{item.heading}</p>
                 <Separator className="text-orange-400 mb-4"/>
                 <p className="px-2 text-start md:py-8 shadow font-montserrat text-gray-500 dark:text-white tracking-wide mb-6">
-                  {item.text}
+                  {isFullTextVisible ? item.text : truncateText(item.text, textLimit)}
+                  <button 
+                    className="inline text-xs xs:text-sm text-blue-400 underline cursor-pointer"
+                    onClick={()=>toggleVisibility(item.id)}
+                    >
+                      {isFullTextVisible?'See less':'See more'}
+                    </button>
                 </p>
             </div>
             <div className={`flex justify-center items-center ${i % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
