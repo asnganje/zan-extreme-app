511f98570a877a9fd5226b3f45dddadd5bb55a2c
asnganje <abd.nganje@gmail.com>
2024-09-05 09:29:04 +0300

    Modify text padding

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,15 +4,15 @@ import { Separator } from "../ui/separator"
 function Dashboard() {
   return (
     <div className="shadow-2xl my-[12vh] p-8">
-      <p className="text-center uppercase text-sm xs:text-2xl md:text-lg lg:text-3xl text-blue-300 py-10 shadow-2xl rounded my-10">"Discover Zanzibar: Wonders Tour Package"</p>
+      <p className="text-center uppercase text-xl xs:text-3xl text-blue-300 py-8 shadow-2xl rounded mt-2 mb-10"><span className="hidden md:inline">"Discover Zanzibar: </span>Wonders Tour Package<span className="hidden md:inline">"</span></p>
       {
         dashItems.map((item, i)=> {
           return (
             <div key={item.id} className={`flex flex-col md:grid md:grid-cols-2 md:gap-[5vw] mb-[15vh]`}>
               <div className={`flex flex-col ${i % 2 === 0 ? 'md:order-2' : 'md:order-1'}`}>
                 <p className="text-blue-300 text-center text-2xl cursor-pointer">{item.heading}</p>
-                <Separator className="text-orange-400 w-[80%] flex justify-center items-center"/>
-                <p className="px-8 text-start md:py-8 shadow font-montserrat text-gray-500 dark:text-white tracking-wide">{item.text}</p>
+                <Separator className="text-orange-400 mb-4"/>
+                <p className="px-2 text-start md:py-8 shadow font-montserrat text-gray-500 dark:text-white tracking-wide mb-6">{item.text}</p>
             </div>
             <div className={`flex justify-center items-center ${i % 2 === 0 ? 'md:order-1' : 'md:order-2'}`}>
               <img src={item.image} alt="" className="cursor-pointer"/>
