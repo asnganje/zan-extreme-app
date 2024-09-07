b38822e666713306b370f8e94b0851dae4079369
asnganje <abd.nganje@gmail.com>
2024-09-07 10:18:45 +0300

    Fix bg-color

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom"
 
 function HomeLayout() {
   return (
-    <div className="flex flex-col align-element pt-10 pb-5">
+    <div className="flex flex-col align-element pb-2 bg-white">
       <Header />
       <Navbar />
       <Outlet />
