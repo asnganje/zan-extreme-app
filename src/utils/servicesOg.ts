cdbdca711ce4681fb1860117d4d76d33ffa85a99
asnganje <abd.nganje@gmail.com>
2024-09-08 19:17:15 +0300

    Created services list

---
+++ src/utils/servicesOg.ts
@@ -0,0 +1,53 @@
+import { RService } from "./types";
+import gt from "/groundTransfer.png"
+import islT from "/prisonIsland.png"
+import hipos from "/hippos.png"
+import incentv from "/landingImg2.png"
+import royal from "/royal.png"
+import conference from "/conference.png"
+import real from "/real.png"
+
+export const servicesR: RService[] = [
+  {
+    id: 1,
+    img:gt,
+    heading: 'Luxurious Ground Transfers',
+    content: 'Seamless transportation to and from the airport, hotels, and beyond in style and comfort.',
+  },
+  {
+    id: 2,
+    img:islT,
+    heading: 'Island Tours',
+    content: 'Explore pristine islands with expert guides, discovering hidden gems, local culture, and breathtaking landscapes in unparalleled luxury.',
+  },
+  {
+    id: 3,
+    img:hipos,
+    heading: 'Wilderness Safaris',
+    content: 'Experience the thrill of the wild with exclusive safari adventures, offering close encounters with nature in comfort and safety.',
+  },
+  {
+    id: 4,
+    img:incentv,
+    heading: 'Incentive Group Adventures',
+    content: 'Seamless transportation to and from the airport, hotels, and beyond in style and comfort.',
+  },
+  {
+    id: 5,
+    img:royal,
+    heading: 'Enchanting Royal and Traditional Weddings',
+    content: 'Expertly crafted ceremonies in breathtaking settings, turning your dream wedding into reality.',
+  },
+  {
+    id: 6,
+    img:conference,
+    heading: 'Prestigious Conferences',
+    content: 'Tailored conference arrangements in stunning locations, designed to inspire and impress.',
+  },
+  {
+    id: 7,
+    img:real,
+    heading: 'Elite Consultation and Real Estate Services',
+    content: 'Personalized guidance and expertise to help you navigate the local market with confidence.',
+  },
+]
