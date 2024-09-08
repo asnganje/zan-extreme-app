608511bdbb931e81a51021d2e7ca0f06a5bc9ea9
asnganje <abd.nganje@gmail.com>
2024-09-08 19:18:09 +0300

    Modify about page content

--- src/components/About/AboutContext.tsx
+++ src/components/About/AboutContext.tsx
@@ -3,10 +3,10 @@ function AboutContext() {
     <div className="font-robotoSlab bg-white dark:bg-black w-[70vw] mx-auto px-4 py-8 shadow rounded tracking-loose">
       <div className="flex flex-col gap-8 mb-8">
         <p className="text-[16px]">
-        <span className="text-2xl">ZAN EXTREME HOLIDAYS AND WILDLIFE SAFARIS </span> is a premier luxury travel company specializing in crafting personalized trips and holiday experiences in the Zanzibar archipelago, known as the "Spice Islands," and mainland Tanzania. With over 15 years of expertise in the Tourism and Hospitality industry, we are dedicated to delivering exceptional customer service across the renowned Southern and Northern Safari Circuits, ensuring that each journey with us is truly unforgettable.
+        <span className="text-xl">ZAN EXTREME HOLIDAYS AND WILDLIFE SAFARIS </span> is a premier luxury travel company specializing in crafting personalized trips and holiday experiences in the Zanzibar archipelago, known as the "Spice Islands," and mainland Tanzania. With over 15 years of expertise in the Tourism and Hospitality industry, we are dedicated to delivering exceptional customer service across the renowned Southern and Northern Safari Circuits, ensuring that each journey with us is truly unforgettable.
         </p>
         <p className="text-[16px]">
-        Our operations are well-equipped with luxury vans, minibuses, and royal wagons in Zanzibar, along with 4x4 Safari Jeeps stationed in Arusha and Dar es Salaam, ensuring seamless ground handling, transfers, and excursions. Our commitment to quality and comfort guarantees that every journey with us is smooth and enjoyable.
+        Our operations are well-equipped with luxury vans, minibuses, and royal wagons in Zanzibar, ensuring seamless ground handling, transfers, and excursions, along with 4x4 Safari Jeeps stationed in Arusha and Dar es Salaam. Our commitment to quality and comfort guarantees that every journey with us is smooth and enjoyable.
         </p>
       </div>
       <div className="flex flex-col gap-8 mb-8">
@@ -24,6 +24,8 @@ function AboutContext() {
         <p className="text-orange-400 text-xl">Our Exclusive Offerings:</p>
         <ul className="ml-[5vw] flex flex-col gap-2">
           <li className="list-disc">Luxurious Ground Transfers</li>
+          <li className="list-disc">Island Tours</li>
+          <li className="list-disc">Wilderness Safaris</li>
           <li className="list-disc">Enchanting Royal and Traditional Weddings</li>
           <li className="list-disc">Prestigious Conferences</li>
           <li className="list-disc">Incentive Group Adventures</li>
