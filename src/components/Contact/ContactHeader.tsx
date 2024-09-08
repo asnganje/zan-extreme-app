c69c2d0e434bdef9563c4824d9a9efac7d609c4b
asnganje <abd.nganje@gmail.com>
2024-09-08 19:18:30 +0300

    Replaced address icon in contact header

--- src/components/Contact/ContactHeader.tsx
+++ src/components/Contact/ContactHeader.tsx
@@ -1,6 +1,6 @@
 import { FcCellPhone } from "react-icons/fc";
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
-import { FaRegAddressBook } from "react-icons/fa6";
+import { CiLocationOn } from "react-icons/ci";
 
 function ContactHeader() {
   return (
@@ -8,7 +8,8 @@ function ContactHeader() {
       <div className="flex flex-col justify-center items-center gap-2 text-[15px">
         <FcCellPhone className="w-[10vw] h-[10vh]"/>
         <h3 className="font-bold font-serif leading-8 tracking-wider text-orange-400 text-lg">Telephone</h3>
-        <p className="text-gray-600 leading-6 dark:text-white">+255 765 118 898</p>
+        <p className="text-gray-600 dark:text-white -mb-2">+255 765 118 898</p>
+        <p className="text-gray-600 dark:text-white">+255 762 213 399</p>
       </div>
       <div className="flex flex-col justify-center items-center gap-2 text-[15px">
         <MdOutlineMarkEmailUnread className="w-[10vw] h-[10vh] text-blue-400"/>
@@ -16,7 +17,7 @@ function ContactHeader() {
         <p className="text-gray-600 leading-6 dark:text-white">zanextremeholydays@gmail.com</p>
       </div>
       <div className="flex flex-col justify-center items-center gap-2 text-[15px]">
-        <FaRegAddressBook className="w-[10vw] h-[10vh] text-blue-700"/>
+        <CiLocationOn className="w-[10vw] h-[10vh] text-blue-700"/>
         <h3 className="font-bold font-serif leading-8 tracking-wider text-orange-400 text-lg">Address</h3>
         <p className="text-gray-600 leading-6 dark:text-white">Magomeni Wandaras, Zanzibar</p>
       </div>
