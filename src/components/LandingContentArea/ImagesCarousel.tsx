f51345db3f606d82fe212b3ff3abacefe80fb958
asnganje <abd.nganje@gmail.com>
2024-09-13 12:14:37 +0300

    Modify images carousel

--- src/components/LandingContentArea/ImagesCarousel.tsx
+++ src/components/LandingContentArea/ImagesCarousel.tsx
@@ -4,6 +4,7 @@ import img3 from "/landingImg3.png";
 import { Card, CardHeader } from "../ui/card";
 import { AspectRatio } from "../ui/aspect-ratio";
 import { useEffect, useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react"; // Assuming you're using Lucide icons
 
 const images = [
   { id: 3, src: img3, alt: "landing img3" },
@@ -17,27 +18,52 @@ function ImagesCarousel() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prevIndex: number) => (prevIndex + 1) % images.length);
-    }, 45000);
+    }, 4500);
 
     return () => clearInterval(intervalId);
   }, []);
+
+  const goToImage = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="relative w-full h-[30vh] xs:h-[40vh] md:h-[80vh] overflow-hidden">
+      {/* Arrows stay fixed here */}
+      <button
+        onClick={prevImage}
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 p-2 bg-gray-700 text-white rounded-full hover:bg-gray-800 z-30"
+      >
+        <ChevronLeft size={30} />
+      </button>
+
+      <button
+        onClick={nextImage}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 p-2 bg-gray-700 text-white rounded-full hover:bg-gray-800 z-30"
+      >
+        <ChevronRight size={30} />
+      </button>
+
+      {/* Images container */}
       {images.map((image, index) => (
         <div
           key={image.id}
-          className={`absolute inset-0  transition-transform duration-1000 ease-in-out ${
-            currentIndex === index
-              ? "transform translate-x-0 z-10"
-              : "transform translate-x-full z-0"
-          } ${
-            currentIndex === (index + 1) % images.length
-              ? "transform -translate-x-1/2 z-20"
-              : ""
+          className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
+            currentIndex === index ? "opacity-100 z-10" : "opacity-0 z-0"
           }`}
         >
           <Card className="h-full w-full">
-            <CardHeader className="!p-0 ">
+            <CardHeader className="!p-0">
               <AspectRatio ratio={16 / 9}>
                 <img
                   src={image.src}
@@ -49,7 +75,23 @@ function ImagesCarousel() {
           </Card>
         </div>
       ))}
+
+      {/* Dots navigation */}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2 z-20">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => goToImage(index)}
+            className={`w-3 h-3 md:w-4 md:h-4 rounded-full transition-all duration-300 ${
+              currentIndex === index
+                ? "bg-blue-500 scale-125"
+                : "bg-gray-400"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
 export default ImagesCarousel;
