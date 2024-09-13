8f9afb4bf8e865fb5b1795a079f27f7fb0a0391c
asnganje <abd.nganje@gmail.com>
2024-09-13 20:35:45 +0300

    update json and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  base: '/zan-extreme-app/',
+});
